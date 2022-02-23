3da30b69a9db9b92f38de2d994848813975f4a3f
ahmedmohamed520 <frontenddeveloper12020@gmail.com>
2022-02-23 23:57:20 +0200

    initial comment

---
+++ script.js
@@ -0,0 +1,15 @@
+"use strict";
+const advice = document.getElementById("advice");
+const id = document.getElementById("id");
+const roll = document.querySelector(".dice");
+const getData = async () => {
+    const res = await fetch("https://api.adviceslip.com/advice");
+    const data = await res.json();
+    advice.textContent = '"' + data.slip.advice + '"';
+    id.textContent = "#" + data.slip.id;
+    console.log(data.slip.advice);
+};
+getData();
+roll.addEventListener("click", () => {
+    getData();
+});
