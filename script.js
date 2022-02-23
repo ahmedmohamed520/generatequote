74dcd0741bb8d97312bb1bdfbfa324460841dde0
ahmedmohamed520 <85392258+ahmedmohamed520@users.noreply.github.com>
2022-02-24 00:30:53 +0200

    Update script.js

--- script.js
+++ script.js
@@ -5,6 +5,10 @@ const roll = document.querySelector(".dice");
 const getData = async () => {
     const res = await fetch("https://api.adviceslip.com/advice");
     const data = await res.json();
+    if (advice.textContent === data.slip.advice) {
+        getData();
+        return;
+    }
     advice.textContent = '"' + data.slip.advice + '"';
     id.textContent = "#" + data.slip.id;
     console.log(data.slip.advice);
